80148907451d0c30ac42cb629ec5750c999d3c17
Abdullah Joud <joud@kfupm.edu.sa>
2016-05-20 22:27:42 +0300

    forms and views re-structuring

--- qabool/undergraduate_admission/urls.py
+++ qabool/undergraduate_admission/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout
 
-from . import views
+from .views import general_views, phase1_views
 
 urlpatterns = [
     url(
         r'^login/$',
         # login,
-        views.index,
+        general_views.index,
         name='login',
         # kwargs={'template_name': 'undergraduate_admission/login.html'}
     ),
@@ -17,10 +17,10 @@ urlpatterns = [
         name='logout',
         kwargs={'next_page': '/'}
     ),
-    url(r'^register/$', views.RegisterView.as_view(), name='register'),
-    url(r'^studentarea/$', views.student_area, name='student_area'),
-    url(r'^registrationsuccess/$', views.registration_success, name='registration_success'),
-    url(r'^initialagreement/$', views.initial_agreement, name='initial_agreement'),
-    url(r'^forgotpassword/$', views.forgot_password, name='forgot_password'),
-    url(r'^$', views.index, name='index'),
+    url(r'^register/$', phase1_views.RegisterView.as_view(), name='register'),
+    url(r'^studentarea/$', general_views.student_area, name='student_area'),
+    url(r'^registrationsuccess/$', phase1_views.registration_success, name='registration_success'),
+    url(r'^initialagreement/$', phase1_views.initial_agreement, name='initial_agreement'),
+    url(r'^forgotpassword/$', general_views.forgot_password, name='forgot_password'),
+    url(r'^$', general_views.index, name='index'),
 ]
--- qabool/undergraduate_admission/views/phase1_views.py
+++ qabool/undergraduate_admission/views/phase1_views.py
@@ -1,42 +1,13 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.cache import cache_page
-
-from django.views.generic.edit import CreateView
-from django.http import HttpResponse, Http404
-from django.core.urlresolvers import reverse_lazy, reverse
-from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect, get_object_or_404, render
+from django.views.decorators.cache import cache_page
+from django.views.generic import CreateView
 from django.utils.translation import ugettext_lazy as _
 
-from .models import User, RegistrationStatusMessage, AdmissionSemester, Agreement
-from .forms import RegistrationForm, MyAuthenticationForm, ForgotPasswordForm, AgreementForm
-from .utils import SMS, Email
-
-
-def index(request, template_name='undergraduate_admission/login.html'):
-    if request.method == 'GET' and request.user.is_authenticated():
-        return redirect(reverse('student_area'))
-
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-
-                if user.is_superuser:
-                    return redirect('/admin')
-                else:
-                    return redirect(reverse('student_area'))
-
-        else:
-            form = MyAuthenticationForm(request.POST)
-    else:
-        form = MyAuthenticationForm()
-
-    return render(request, template_name, {'form': form})
+from undergraduate_admission.forms.phase1_forms import AgreementForm, RegistrationForm
+from undergraduate_admission.models import User, RegistrationStatusMessage, AdmissionSemester, Agreement
+from undergraduate_admission.utils import SMS, Email
 
 
 @cache_page(60 * 15)
@@ -105,24 +76,3 @@ def registration_success(request):
         return render(request, 'undergraduate_admission/registration_success.html', context={'user': user})
     else:
         return redirect('register')
-
-
-def forgot_password(request):
-    form = ForgotPasswordForm(request.POST or None)
-
-    if request.method == 'POST':
-        if form.is_valid():
-            saved = form.save()
-            if saved:
-                messages.success(request, _('Password was reset successfully...'))
-                return redirect(reverse('index'))
-            else:
-                messages.error(request, _('Error resetting password. Make sure you enter the correct info.'))
-
-    return render(request, 'undergraduate_admission/forgot_password.html', {'form': form})
-
-@login_required
-def student_area(request):
-    return render(request, 'undergraduate_admission/student_area.html', context={'user': request.user})
-
-
