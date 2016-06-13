e288f2dbe0939f228f76772a12f14eb1782d3f7a
Abdullah Joud <joud@kfupm.edu.sa>
2016-06-13 14:29:08 +0300

    improved admin export/emport for User

--- qabool/undergraduate_admission/admin.py
+++ qabool/undergraduate_admission/admin.py
@@ -17,7 +17,7 @@ class UserResource(resources.ModelResource):
                   'birthday', 'birthday_ah', 'high_school_graduation_year', 'kfupm_id', 'first_name_ar',
                   'second_name_ar', 'third_name_ar', 'family_name_ar', 'first_name_en', 'second_name_en',
                   'third_name_en', 'family_name_en', 'high_school_name', 'high_school_system',
-                  'high_school_province',)
+                  'high_school_province', 'admission_letter_note', 'admission_note', )
         skip_unchanged = True
         report_skipped = True
 
