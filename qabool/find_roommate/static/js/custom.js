29140a218dd1ea022e2c7e6c2897d39f1524d72e
Abdullah Joud <joud@kfupm.edu.sa>
2016-08-10 12:21:12 +0300

    - improved the UI/UX of the housing update form

--- qabool/find_roommate/static/js/custom.js
+++ qabool/find_roommate/static/js/custom.js
@@ -1,3 +1,16 @@
-$(function(){
+$(function () {
     $("input[type=checkbox]").removeClass("form-control");
+    searchable($("#id_searchable").val());
+    $("#id_searchable").change(function () {
+        searchable($(this).val());
+    })
 });
+
+function searchable(val) {
+    if (val == 'False') {
+        $(".form_update").hide();
+    }
+    else {
+        $(".form_update").show();
+    }
+}
