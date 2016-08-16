5b7cabb8099dd4dcc8117860325670e7cab4b99c
Abdullah Joud <joud@kfupm.edu.sa>
2016-08-16 17:07:24 +0300

    added dynamic housing documents functionality

--- qabool/find_roommate/urls.py
+++ qabool/find_roommate/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import housing_info_update, housing_search, PostList
+from .views import *
 from .models import User
 
 urlpatterns = [
     url(r'^findroommate/$', housing_info_update, name='housing_info_update'),
-    # url(r'^housingsearch/$', PostList.as_view(), name='housing_search'),
     url(r'^housingsearch/$', housing_search, name='housing_search'),
+    url(r'^housingletter2/$', housing_letter2, name='housing_letter2'),
+    url(r'^housingletter1/$', housing_letter1, name='housing_letter1'),
 ]
