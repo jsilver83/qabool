ac4e6cdbb328df0a774f79990bcbb47f3d5b8a9e
Abdullah Joud <joud@kfupm.edu.sa>
2016-08-18 14:13:21 +0300

    hosuing user admin

--- qabool/find_roommate/admin.py
+++ qabool/find_roommate/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from find_roommate.models import HousingUser
+
+
+class UserHousingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'facebook', 'twitter', 'sleeping', 'light',
+                    'room_temperature', 'visits', 'searchable')
+
+    list_filter = ('searchable', 'user__eligible_for_housing',)
+
+
+admin.site.register(HousingUser, UserHousingAdmin)
