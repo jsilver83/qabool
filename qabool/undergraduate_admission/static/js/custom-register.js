fe9c2a52d74c5e3f28ddf83733584c468d2076a8
Abdullah Joud <joud@kfupm.edu.sa>
2017-05-11 15:48:30 +0300

    fixed no-copy on confirmation for email, mobile, and gov id removed quodrat and tahsili from registration form

--- qabool/undergraduate_admission/static/js/custom-register.js
+++ qabool/undergraduate_admission/static/js/custom-register.js
@@ -1,30 +1,35 @@
-$(function(){
+$(function () {
     $(".subBtn").hide();
     $('.concurr').prop('checked', false);
-    $(".concurr").click(function(){
+    $(".concurr").click(function () {
         $(".subBtn").toggle();
     });
 
     sm($("#id_nationality").prop('selectedIndex'));
-    $("#id_nationality").change(function(){
+    $("#id_nationality").change(function () {
         var vaa = $(this).prop('selectedIndex');
         sm(vaa);
     });
     $("textarea").addClass("form-control");
+
+    $('.nocopy').bind('copy paste cut', function (e) {
+        e.preventDefault(); //disable cut,copy,paste
+        //alert('cut,copy & paste options are disabled !!');
+    });
 });
 
-function sm(nat){
-    if(nat == 1){
+function sm(nat) {
+    if (nat == 1) {
         $("#id_saudi_mother").parents(".form-group").hide();
         $("#id_saudi_mother").val("");
         $("#id_saudi_mother").removeAttr("required");
     }
-    else if(nat == null || nat == ""){
+    else if (nat == null || nat == "") {
         $("#id_saudi_mother").parents(".form-group").hide();
     }
-    else{
+    else {
         $("#id_saudi_mother").parents(".form-group").show();
-        if($("#id_saudi_mother").val() != 'True'){
+        if ($("#id_saudi_mother").val() != 'True') {
             $("#id_saudi_mother").val("False");
         }
         $("#id_saudi_mother").attr("required", "");
