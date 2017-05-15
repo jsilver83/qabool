830fe760e042d560d665d8120a9dbf4c5254717c
Bassam Almaaesh <almaaesh@kfupm.edu.sa>
2017-05-15 10:45:31 +0300

    create form for phase 3 and added new field submit_date for current phase

--- qabool/undergraduate_admission/forms/phase3_forms.py
+++ qabool/undergraduate_admission/forms/phase3_forms.py
@@ -1,25 +1,17 @@
 import floppyforms.__future__ as forms
 from django.utils import timezone
 
-from undergraduate_admission.models import User, Lookup, RegistrationStatusMessage
+from undergraduate_admission.models import User, Lookup, RegistrationStatusMessage, TarifiReceptionDate
 from django.utils.translation import ugettext_lazy as _, get_language
-
-YES_NO_CHOICES = (
-    ('True', _("Yes")),
-    ('False', _("No")),
-)
 
 
 class TarifiTimeSlotForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['id', ]
+        fields = ['tarifi_week_attendance_date', ]
 
-    def save(self, commit=True):
-        instance = super(TarifiTimeSlotForm, self).save(commit=False)
-        instance.phase2_submit_date = timezone.now()
-        if commit:
-            instance.save()
-            return instance
-        else:
-            return instance
+    def __init__(self, *args, **kwargs):
+        super(TarifiTimeSlotForm, self).__init__()
+        self.user = kwargs.pop('user')
+        self.fields['tarifi_week_attendance_date'].widget = forms.Select(choices=TarifiReceptionDate.
+                                                    get_all_available_slots(self.user, add_dashes=True))
