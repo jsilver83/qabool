5ce6277c236eb17a29a01160247c1df92d5c03e7
Bassam Almaaesh <almaaesh@kfupm.edu.sa>
2017-05-18 11:59:06 +0300

    Modify javascript to show and hide id_saudi_mother_gov_id

--- qabool/undergraduate_admission/static/js/custom-register.js
+++ qabool/undergraduate_admission/static/js/custom-register.js
@@ -10,6 +10,10 @@ $(function () {
         var vaa = $(this).prop('selectedIndex');
         sm(vaa);
     });
+    $("#id_saudi_mother").change(function () {
+        var vaa = $("#id_nationality").prop('selectedIndex');
+        sm(vaa);
+    });
     $("textarea").addClass("form-control");
 
     $('.nocopy').bind('copy paste cut', function (e) {
@@ -23,15 +27,29 @@ function sm(nat) {
         $("#id_saudi_mother").parents(".form-group").hide();
         $("#id_saudi_mother").val("");
         $("#id_saudi_mother").removeAttr("required");
+
+        $("#id_saudi_mother_gov_id").parents(".form-group").hide();
+        $("#id_saudi_mother_gov_id").val("");
+        $("#id_saudi_mother_gov_id").removeAttr("required");
     }
     else if (nat == null || nat == "") {
         $("#id_saudi_mother").parents(".form-group").hide();
+        $("#id_saudi_mother_gov_id").parents(".form-group").hide();
     }
     else {
         $("#id_saudi_mother").parents(".form-group").show();
-        if ($("#id_saudi_mother").val() != 'True') {
-            $("#id_saudi_mother").val("False");
-        }
         $("#id_saudi_mother").attr("required", "");
+
+        if ($("#id_saudi_mother").val() == 'True') {
+            $("#id_saudi_mother").val("True");
+            $("#id_saudi_mother_gov_id").parents(".form-group").show();
+            $("#id_saudi_mother_gov_id").attr("required", "");
+        }
+        else{
+            $("#id_saudi_mother").val("False");
+            $("#id_saudi_mother_gov_id").val("");
+            $("#id_saudi_mother_gov_id").parents(".form-group").hide();
+            $("#id_saudi_mother_gov_id").removeAttr("required");
+        }
     }
 }
