cfe7c1d6dc31bbc48a97bf4ce2459c420c00983d
Bassam Almaaesh <almaaesh@kfupm.edu.sa>
2017-05-23 12:49:40 +0300

    Add new field called roommate_id in the model and view. Add chooseroommate in the urls. And generate translation file

--- qabool/undergraduate_admission/forms/phase3_forms.py
+++ qabool/undergraduate_admission/forms/phase3_forms.py
@@ -8,4 +8,21 @@ from django.utils.translation import ugettext_lazy as _, get_language
 class TarifiTimeSlotForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['tarifi_week_attendance_date', ]
+        fields = ['tarifi_week_attendance_date']
+
+
+class ChooseRoommateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['roommate_id']
+
+    def clean_roommate_id(self):
+        roommate_id = self.cleaned_data.get("roommate_id")
+        if not User.objects.filter(username=roommate_id,
+                               eligible_for_housing=True,
+                               status_message=RegistrationStatusMessage.get_status_admitted()):
+            raise forms.ValidationError(
+                _('This ID does not exist.'),
+                code='roommate_does_not_exist',
+            )
+        return roommate_id
