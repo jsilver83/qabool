9b5b1fc0a8da390ed0e3e940b0fbd2f84b8b81f4
Abdullah Joud <joud@kfupm.edu.sa>
2017-06-08 16:13:45 +0300

    fixed redirect after submit to same page

--- qabool/undergraduate_admission/views/admin_side_views.py
+++ qabool/undergraduate_admission/views/admin_side_views.py
@@ -1,14 +1,13 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.core.urlresolvers import reverse_lazy
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, render_to_response
+from django.template import RequestContext
+from django.utils.translation import ugettext_lazy as _
 from django.views.generic import TemplateView
 from django.views.generic import UpdateView, View
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from extra_views import FormSetView
 from extra_views import ModelFormSetView
 from floppyforms.__future__ import modelformset_factory
 
@@ -17,25 +16,6 @@ from undergraduate_admission.forms.admin_side_forms import CutOffForm, VerifyCom
     StudentGenderForm
 from undergraduate_admission.models import User, AdmissionSemester
 from undergraduate_admission.utils import try_parse_float
-from django.utils.translation import ugettext_lazy as _
-
-
-# def cut_off_point(request):
-#     form = CutOffForm(request.GET or None)
-#     selected_student_types = request.GET.getlist('student_type', ['S', 'M', 'N'])
-#     print(selected_student_types)
-#     cut_off_total = try_parse_float(request.GET.get('admission_total', 0.0))
-#     print(cut_off_total)
-#
-#     filtered = UserListFilter(request.GET, queryset=User.objects.all())
-#     filtered_with_properties = [student for student in filtered.qs
-#                                 if student.student_type in selected_student_types]
-#     filtered_with_properties2 = [student for student in filtered_with_properties
-#                                  if student.admission_total >= cut_off_total]
-#
-#     return render(request,
-#                   template_name='undergraduate_admission/admin/cutoff.html',
-#                   context={'form': form, 'students': filtered_with_properties2})
 
 
 class AdminBaseView(LoginRequiredMixin, UserPassesTestMixin):
@@ -124,15 +104,17 @@ class StudentGenderViewNOTWORKING(AdminBaseView, SuccessMessageMixin, ModelFormS
 
 class StudentGenderView(AdminBaseView, TemplateView):
     formset = modelformset_factory(User, form=StudentGenderForm, extra=0)
+    template_name = 'undergraduate_admission/admin/student_gender.html'
 
-    def get(self, request, *args, **kwargs):
+    def get_context_data(self, **kwargs):
+        context = super(StudentGenderView, self).get_context_data(**kwargs)
         sem = AdmissionSemester.get_phase1_active_semester()
         students =  User.objects.filter(semester=sem,
                                         is_staff=False,
                                         is_superuser=False).order_by('date_joined')
 
-        paginator = Paginator(students, 25)
-        page = request.GET.get('page')
+        paginator = Paginator(students, 1)
+        page = self.request.GET.get('page')
         try:
             page_obj = paginator.page(page)
         except PageNotAnInteger:
@@ -141,9 +123,15 @@ class StudentGenderView(AdminBaseView, TemplateView):
             page_obj = paginator.page(paginator.num_pages)
         page_query = students.filter(id__in=[object.id for object in page_obj])
         formset = self.formset(queryset=page_query)
-        context = {'page_obj': page_obj, 'paginator': paginator, 'formset': formset, 'is_paginated': True}
+        context['page_obj'] = page_obj
+        context['paginator'] = paginator
+        context['formset'] = formset
+        context['is_paginated'] = True
 
-        return render(request, 'undergraduate_admission/admin/student_gender.html', context)
+        return context
+
+    # def get(self, request, *args, **kwargs):
+    #     return render(request, self.template_name, self.get_context_data())
 
     def post(self, request, *args, **kwargs):
         formset = self.formset(request.POST)
@@ -153,4 +141,7 @@ class StudentGenderView(AdminBaseView, TemplateView):
                 form.save()
             messages.success(request, _('Gender updated successfully'))
 
-        return HttpResponseRedirect("")
+            return render_to_response(self.template_name,
+                                      self.get_context_data(**kwargs),
+                                      context_instance=RequestContext(request))
+        # return HttpResponseRedirect("")
