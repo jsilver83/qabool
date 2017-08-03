aa90768258802d6602607ea050d5a144a111d304
Abdullah Joud <joud@kfupm.edu.sa>
2017-08-03 10:24:28 +0300

    find roommate functionality (first trial)

--- qabool/find_roommate/admin.py
+++ qabool/find_roommate/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from find_roommate.models import HousingUser
+from find_roommate.models import HousingUser, Room, RoommateRequest
 
 
 class UserHousingAdmin(admin.ModelAdmin):
@@ -10,4 +10,18 @@ class UserHousingAdmin(admin.ModelAdmin):
     list_filter = ('searchable', 'user__eligible_for_housing',)
 
 
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'building', 'room', 'available')
+
+    list_filter = ('available', )
+
+
+class RoommateRequestAdmin(admin.ModelAdmin):
+    list_display = ('requesting_user', 'requested_user', 'status', 'assigned_room', 'request_date', 'updated_on')
+
+    list_filter = ('status', )
+
+
 admin.site.register(HousingUser, UserHousingAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(RoommateRequest, RoommateRequestAdmin)
