7ea3404c67ba90ea20a9e38aff0a306b0afddab5
Abdullah Joud <joud@kfupm.edu.sa>
2017-08-08 16:40:39 +0300

    fixed runtime error in signal after staff login

--- qabool/find_roommate/signals.py
+++ qabool/find_roommate/signals.py
@@ -8,15 +8,18 @@ from undergraduate_admission.models import User
 @receiver(post_save, sender=User)
 def student_post_withdrawal(sender, **kwargs):
     user = kwargs['instance']
-    if user.status_message.status.status_code == 'WITHDRAWN':
-        roommate_requests = RoommateRequest.objects.filter(requesting_user=user,
-                                                           status__in=[
-                                                               RoommateRequest.RequestStatuses.PENDING,
-                                                               RoommateRequest.RequestStatuses.ACCEPTED])
-        roommate_requests.update(status=RoommateRequest.RequestStatuses.REQUESTING_STUDENT_WITHDRAWN)
+    try:
+        if user.status_message.status.status_code == 'WITHDRAWN':
+            roommate_requests = RoommateRequest.objects.filter(requesting_user=user,
+                                                               status__in=[
+                                                                   RoommateRequest.RequestStatuses.PENDING,
+                                                                   RoommateRequest.RequestStatuses.ACCEPTED])
+            roommate_requests.update(status=RoommateRequest.RequestStatuses.REQUESTING_STUDENT_WITHDRAWN)
 
-        roommate_requests = RoommateRequest.objects.filter(requested_user=user,
-                                                           status__in=[
-                                                               RoommateRequest.RequestStatuses.PENDING,
-                                                               RoommateRequest.RequestStatuses.ACCEPTED])
-        roommate_requests.update(status=RoommateRequest.RequestStatuses.REQUESTED_STUDENT_WITHDRAWN)
+            roommate_requests = RoommateRequest.objects.filter(requested_user=user,
+                                                               status__in=[
+                                                                   RoommateRequest.RequestStatuses.PENDING,
+                                                                   RoommateRequest.RequestStatuses.ACCEPTED])
+            roommate_requests.update(status=RoommateRequest.RequestStatuses.REQUESTED_STUDENT_WITHDRAWN)
+    except AttributeError:  # staff users don't have status
+        pass
