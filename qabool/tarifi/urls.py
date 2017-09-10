f4d097ba88991e25bcdceaf1febcc5e5e591b1b1
Abdullah Joud <joud@kfupm.edu.sa>
2017-09-10 18:38:40 +0300

    changed landing page url to avoid ambiguity

--- qabool/tarifi/urls.py
+++ qabool/tarifi/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     url(r'^simulation/$', TarifiSimulation.as_view(), name='tarifi_simulation'),
     url(r'^student/(?P<pk>\d+)/$', StudentPrintPage.as_view(), name='student_print_page'),
     url(r'^attendance/$', CourseAttendance.as_view(), name='preparation_course_attendance'),
-    url(r'', TarifiLandingPage.as_view(), name='tarifi_landing_page'),
+    url(r'^reception/$', TarifiLandingPage.as_view(), name='tarifi_landing_page'),
 ]
-
