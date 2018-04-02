dff87064de67f28875ec56eef1406dddb108a49b
Abdullah Joud <joud@kfupm.edu.sa>
2018-04-02 16:15:58 +0300

    added check on file size not to exceed 2 MB

--- qabool/undergraduate_admission/validators.py
+++ qabool/undergraduate_admission/validators.py
@@ -4,6 +4,24 @@ import os
 from django.core.exceptions import ValidationError
 
 
+MAX_IMAGE_UPLOAD_SIZE = 2000000
+MAX_FILE_UPLOAD_SIZE = 2000000
+
+
+def size_format(b):
+    b = float(b)
+    if b < 1000:
+        return '%i' % b + 'B'
+    elif 1000 <= b < 1000000:
+        return '%.1f' % float(b / 1000) + ' KB'
+    elif 1000000 <= b < 1000000000:
+        return '%.1f' % float(b / 1000000) + ' MB'
+    elif 1000000000 <= b < 1000000000000:
+        return '%.1f' % float(b / 1000000000) + ' GB'
+    elif 1000000000000 <= b:
+        return '%.1f' % float(b / 1000000000000) + ' TB'
+
+
 # validates file extensions for uploaded documents
 def validate_file_extension(value):
     ext = os.path.splitext(value.name)[1].lower()
@@ -13,6 +31,12 @@ def validate_file_extension(value):
             _('File extension (%(ext)s) not allowed!'),
             params={'ext': ext},
             code='ext_not_allowed',
+        )
+
+    if value.size > MAX_FILE_UPLOAD_SIZE:
+        raise ValidationError(
+            _('File size {} is larger than {}!'.format(size_format(value.size), size_format(MAX_FILE_UPLOAD_SIZE))),
+            code='size_not_allowed',
         )
 
 
@@ -25,6 +49,12 @@ def validate_image_extension(value):
             _('File extension (%(ext)s) not allowed!'),
             params={'ext': ext},
             code='ext_not_allowed',
+        )
+
+    if value.size > MAX_IMAGE_UPLOAD_SIZE:
+        raise ValidationError(
+            _('Image size {} is larger than {}!'.format(size_format(value.size), size_format(MAX_IMAGE_UPLOAD_SIZE))),
+            code='size_not_allowed',
         )
 
 
