de009818703957b0f2be781ba7e9f6ee18eedbd9
Bassam Almaaesh <almaaesh@kfupm.edu.sa>
2018-08-14 13:56:56 +0300

    still there is a problem in logout for the admins and return everything as it was.

--- qabool/qabool/urls.py
+++ qabool/qabool/urls.py
@@ -30,8 +30,6 @@ urlpatterns = i18n_patterns(
     url(r'', include(undergraduate_admission.urls)),
     url(r'', include(find_roommate.urls)),
     url(r'^admin/tarifiweek/', include(tarifi.urls)),
-    url(r'^logout/$', views.logout, {'template_name': 'logout.html'}, name='logout'),
-
 )
 
 urlpatterns += [
