2070c5efac22af3961c5ef57b990168133bab6a0
Abdullah Joud <joud@kfupm.edu.sa>
2019-01-22 10:30:17 +0300

    renamed the variable important_dates to important_dates_list so it wont clash with the function name it resides in.

--- qabool/undergraduate_admission/templatetags/important_dates.py
+++ qabool/undergraduate_admission/templatetags/important_dates.py
@@ -10,11 +10,11 @@ register = template.Library()
 def important_dates(context):
     # cache if important dates is not change for 15 mins
     if cache.get('important_dates') is None:
-        important_dates = ImportantDateSidebar.objects.filter(show=True)
-        cache.set('important_dates', important_dates, 15 * 60)
+        important_dates_list = ImportantDateSidebar.objects.filter(show=True)
+        cache.set('important_dates', important_dates_list, 15 * 60)
     else:
-        important_dates = cache.get('important_dates')
+        important_dates_list = cache.get('important_dates')
 
     return {
-        'important_dates': important_dates,
+        'important_dates': important_dates_list,
     }
