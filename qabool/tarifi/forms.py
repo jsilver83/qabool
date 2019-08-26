a7dcfac41c41244a3d425ee59196bcb2dd85a7c4
Abdullah Joud <joud@kfupm.edu.sa>
2019-08-26 15:19:54 +0300

    cosmetics and added 'attended by' to the traifi reception page

--- qabool/tarifi/forms.py
+++ qabool/tarifi/forms.py
@@ -8,6 +8,10 @@ from shared_app.base_forms import BaseCrispyForm
 class TarifiSearchForm(BaseCrispyForm, forms.Form):
     kfupm_id_gov_id = forms.IntegerField(required=True, label=_('KFUPM ID or Government ID'))
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['kfupm_id_gov_id'].widget.attrs.update({'class': 'form-control'})
+
 
 class ReceptionDeskForm(BaseCrispyForm, forms.Form):
     reception_desk = forms.IntegerField(required=True, label=_('Reception Desk'))
@@ -15,3 +19,7 @@ class ReceptionDeskForm(BaseCrispyForm, forms.Form):
 
 class CourseAttendanceSearchForm(BaseCrispyForm, forms.Form):
     kfupm_id = forms.IntegerField(required=True, label=_('KFUPM ID'))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['kfupm_id'].widget.attrs.update({'class': 'form-control'})
